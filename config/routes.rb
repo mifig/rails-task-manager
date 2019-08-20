b60255c9350d08b6319365d055a7d10e48b49482
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2019-08-20 15:18:23 +0100

    02 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
+
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update', as: :task
+
+  get 'tasks/:id', to: 'tasks#show'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
